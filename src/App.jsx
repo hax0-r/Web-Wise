eed3b705efceeb4952618b48aef7901c1cab363e
Talha <talha18513@gmail.com>
2024-02-23 08:54:52 +0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
 import './Pages/Home/Home.scss'
 import './Components/Navbar/Navbar.scss'
+import './Components/Footer/Footer.scss'
 import Navbar from './Components/Navbar/Navbar'
 import Router from './Routers/Router'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
@@ -10,6 +12,7 @@ function App() {
     <>
     <Navbar/>
     <Router/>
+    <Footer/>
     </>
   )
 }
